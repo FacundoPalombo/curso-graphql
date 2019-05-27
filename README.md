85afc95fb82ed7c6a2e4a9b98534f42a8b7ed4f7
Facundo Palombo <facundo.palombo@gmail.com>
2019-05-27 16:14:10 -0300

    Se configura entorno de servidor y desarrollo

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Esta es una aplicación de prueba para correr GraphQL
 
 * Para instalar las dependencias: `npm install`
 * Para ejecutar el servidor: `npm run start` ó `npm start`
+* Para ejecutar el servidor en modo desarrollo: `npm run dev`
--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-'use strict'
-const { graphql, buildSchema } = require('graphql')
-
+"use strict";
+const { graphql, buildSchema } = require("graphql");
+const express = require("express");
+const app = express();
+const gqlMiddleware = require("express-graphql");
+const port = process.env.PORT || 3000;
+const morgan = require("morgan");
 // definiendo el esquema
 const schema = buildSchema(`
     type Query {
         hello: String,
         saludo: String
     }
-`)
+`);
 
 //configurar los resolvers
 const resolvers = {
-    hello: ()=> {
-        return 'Hello world!'
-    },
-    saludo: ()=> {
-        return 'Hello everyone!'
-    }
-}
+  hello: () => {
+    return "Hello world!";
+  }
+};
+
+//Middlewares
+process.env.NODE_ENV === 'development' ? app.use(morgan('dev')) : app.use(morgan('tiny'));
 
 //Ejecutar el query hello
-graphql(schema, '{hello, saludo}', resolvers).then((data)=> {
-    console.log(data)
-})
+graphql(schema, "{hello}", resolvers).then(data => {
+  console.log(data);
+});
+
+app.use(
+  "/api",
+  gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
