c08a4bbe190b5f05913ba2d652f4d1e37249f09b
Facundo Palombo <facundo.palombo@gmail.com>
2019-05-28 11:14:26 -0300

    Se refactoriza, se crea un esquema y una query para resolver cursos

--- index.js
+++ index.js
@@ -5,23 +5,19 @@ const app = express();
 const gqlMiddleware = require("express-graphql");
 const port = process.env.PORT || 3000;
 const morgan = require("morgan");
-// definiendo el esquema
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        saludo: String
-    }
-`);
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
 
-//configurar los resolvers
-const resolvers = {
-  hello: () => {
-    return "Hello world!";
-  }
-};
+// Leyendo el esquema
+const schema = buildSchema(
+  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
 
 //Middlewares
-process.env.NODE_ENV === 'development' ? app.use(morgan('dev')) : app.use(morgan('tiny'));
+process.env.NODE_ENV === "development"
+  ? app.use(morgan("dev"))
+  : app.use(morgan("tiny"));
 
 //Ejecutar el query hello
 graphql(schema, "{hello}", resolvers).then(data => {
